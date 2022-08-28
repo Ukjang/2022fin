bd06bb5f21603350a486494bec9249f204b4b527
ukjang <bedro4339@gmail.com>
2022-08-28 23:15:31 +0900

    0828_WIL

---
+++ practice_ing/my_package/math/.ipynb_checkpoints/tools-checkpoint.py
@@ -0,0 +1,45 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "42ec0ec8",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# my_package/math/tools.py\n",
+    "\n",
+    "pi = 3.14159265358979323846\n",
+    "\n",
+    "e = 2.71828182845904523536\n",
+    "\n",
+    "def my_max(a, b):\n",
+    "    if a > b:\n",
+    "        return a\n",
+    "    else:\n",
+    "        return b"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
